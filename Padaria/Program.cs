9ae34bddddf635eb21d03e98d72d3c19ab30a2a0
lndsilva <lndsilva@hotmail.com>
2025-03-21 09:50:36 -0300

    calculadora

--- Padaria/Program.cs
+++ Padaria/Program.cs
@@ -16,7 +16,7 @@ namespace Padaria
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmPesquisarFuncionarios());
+            Application.Run(new frmCalculadora());
         }
     }
 }
